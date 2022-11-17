c013ea2fa0244ba034d79dbf3ccde7fe5cbc3320
senaCeejay <senaceejay2425@gmail.com>
2022-11-17 10:35:51 +0800

    remove/input

--- gamerental/resources/views/signup_member.blade.php
+++ gamerental/resources/views/signup_member.blade.php
@@ -1,40 +1,42 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
 
-<form action="/signup_member" method="POST" enctype="multipart/form-data">
+    <form action="/signup_member" method="POST" enctype="multipart/form-data">
         @csrf
-      
-        <label>First name</label>
-        <input type="text" name="fname"></input><br>
-        <label>Last name</label>
-        <input type="text" name="lname"></input><br>
-        <label>Date of Birth</label>
-        <input type="date" name="dob"></input><br>
-        <label>Email</label>
-        <input type="email" name="email"></input><br>
-        <label>Password</label>
-        <input type="password" name="password"></input><br>
-        <label>Confirm Password</label>
-        <input type="password" name="confirmPass"></input><br>
-        <label>Contact Number</label>
-        <input type="text" name="contact"></input><br>
-        <label>Address</label>
-        <input type="text" name="address"></input><br>
-        <label>Upload profile photo:</label>
-        <input type="file" name="profile" accept="{{$allowed_img_format}}"></input>
-        <label>Upload governement ID:</label>
-        <input type="file" name="id"></input>
-        <label>Upload Proof of Billing:</label>
-        <input type="file" name="proof"></input>
-        <button type="submit">Submit</button>
         
+        <label>First name</label>
+        <input type="text" name="fname"><br>
+        <label>Last name</label>
+        <input type="text" name="lname"><br>
+        <label>Date of Birth</label>
+        <input type="date" name="dob"><br>
+        <label>Email</label>
+        <input type="email" name="email"><br>
+        <label>Password</label>
+        <input type="password" name="password"><br>
+        <label>Confirm Password</label>
+        <input type="password" name="confirmPass"><br>
+        <label>Contact Number</label>
+        <input type="text" name="contact"><br>
+        <label>Address</label>
+        <input type="text" name="address"><br>
+        <label>Upload profile photo:</label>
+        <input type="file" name="profile" accept="{{ $allowed_img_format }}">
+        <label>Upload governement ID:</label>
+        <input type="file" name="id">
+        <label>Upload Proof of Billing:</label>
+        <input type="file" name="proof">
+        <button type="submit">Submit</button>
     </form>
 </body>
+
 </html>
