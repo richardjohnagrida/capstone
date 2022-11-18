bb4f52771474cbcd9ee6a930f462f8dbf219dac4
senaCeejay <senaceejay2425@gmail.com>
2022-11-19 00:15:57 +0800

    pushing add game

--- gamerental/resources/views/add_games.blade.php
+++ gamerental/resources/views/add_games.blade.php
@@ -16,13 +16,62 @@
         <main>
 
             <div class="member search">
-                <h1>Member</h1>
+                <h2>Games</h2>
                 <form action="" class="search-bar">
                     <input type="text" placeholder="Search" name="">
                     <button type="submit"><span class="material-icons-sharp">
                             search
                         </span></button>
                 </form>
+            </div>
+            <div class="add-game">
+                <a href="#modal-add-game" class="link-1" id="modal-closed">Add game</a>
+            </div>
+            <div class="modal-container" id="modal-add-game">
+                <div class="modal-add-game">
+                    <form action="add_games" method="POST" enctype="multipart/form-data">
+                        @csrf
+                        <h2>Add Game</h2>
+                        <label>Game name</label>
+                        <input type="text" name="name">
+                        <label>Genre</label>
+                        {{-- <input type="text" name="genre"><br> --}}
+                        <div class="select-genre">
+                            <select name="genre">
+                                <option value="RPG">RPG</option>
+                                <option value="MMORPG">MMORPG</option>
+                                <option value="Strategy">Strategy</option>
+                                <option value="Sports">Sports</option>
+                                <option value="Sports">Simulation</option>
+                                <option value="Adventure">Adventure</option>
+                                <option value="Adventure">Puzzle</option>
+                            </select>
+                        </div>
+                        <label>Console</label>
+                        <div class="select-console">
+                            <select name="console">
+                                @foreach ($platforms as $platform)
+                                    <option value="{{ $platform->console_id }}">
+                                        {{ $platform->console_name }}
+                                    </option>
+                                @endforeach
+                            </select>
+                        </div>
+                        <br>
+                        <label>Price per week</label>
+                        <input type="number" name="week"><br>
+                        <label>Price per month</label>
+                        <input type="number" name="month"><br>
+                        <label>Total Quantity</label>
+                        <input type="number" name="qty"><br>
+                        <label>Description</label>
+                        <input type="text" name="description"><br>
+                        <label for="submit-cover">Cover</label>
+                        <input type="file" name="cover" id="submit-cover" placeholder="Choose file">
+                        <button type="submit" class="modal__btn submit-game">Submit &rarr;</button>
+                    </form>
+                    <a href="#modal-closed" class="link-2"></a>
+                </div>
             </div>
 
             <div class="recent-renter member">
@@ -44,35 +93,39 @@
                             <td>atejo@gmail.com</td>
                             <td class="success" style="color: rgb(3, 199, 3);">Active</td>
                             <td class="primary">
-                                <a href="#modal-opened" class="gamelink-1" id="modal-closed">Update</a>
+                                <a href="#modal-update-game" class="link-1" id="modal-closed">Update</a>
                             </td>
                         </tr>
-                        <div class="modal-container" id="modal-opened">
-                            <div class="modal">
+                        <div class="modal-container" id="modal-update-game">
+                            <div class="modal-game">
                                 <form action="add_games" method="POST" enctype="multipart/form-data">
                                     @csrf
                                     <h2>Add Game</h2>
                                     <label>Game name</label>
-                                    <input type="text" name="name"><br>
+                                    <input type="text" name="name">
                                     <label>Genre</label>
                                     {{-- <input type="text" name="genre"><br> --}}
-                                    <select name="genre">
-                                        <option value="RPG">RPG</option>
-                                        <option value="MMORPG">MMORPG</option>
-                                        <option value="Strategy">Strategy</option>
-                                        <option value="Sports">Sports</option>
-                                        <option value="Sports">Simulation</option>
-                                        <option value="Adventure">Adventure</option>
-                                        <option value="Adventure">Puzzle</option>
-                                    </select>
+                                    <div class="select-genre">
+                                        <select name="genre">
+                                            <option value="RPG">RPG</option>
+                                            <option value="MMORPG">MMORPG</option>
+                                            <option value="Strategy">Strategy</option>
+                                            <option value="Sports">Sports</option>
+                                            <option value="Sports">Simulation</option>
+                                            <option value="Adventure">Adventure</option>
+                                            <option value="Adventure">Puzzle</option>
+                                        </select>
+                                    </div>
                                     <label>Console</label>
-                                    
-                                    <select name="console">
-                                        @foreach ($platforms as $platform)
-                                            <option value="{{ $platform->console_id }}">{{ $platform->console_name }}
-                                            </option>
-                                        @endforeach
-                                    </select>
+                                    <div class="select-console">
+                                        <select name="console">
+                                            @foreach ($platforms as $platform)
+                                                <option value="{{ $platform->console_id }}">
+                                                    {{ $platform->console_name }}
+                                                </option>
+                                            @endforeach
+                                        </select>
+                                    </div>
                                     <br>
                                     <label>Price per week</label>
                                     <input type="number" name="week"><br>
@@ -82,11 +135,11 @@
                                     <input type="number" name="qty"><br>
                                     <label>Description</label>
                                     <input type="text" name="description"><br>
-                                    <label>Cover</label>
-                                    <input type="file" name="cover">
-                                    <button type="submit" class="modal__btn submit-game">Submit</button>
+                                    <label for="submit-cover">Cover</label>
+                                    <input type="file" name="cover" id="submit-cover" placeholder="Choose file">
+                                    <button type="submit" class="modal__btn submit-game">Submit &rarr;</button>
                                 </form>
-                                <a href="#modal-closed" class="gamelink-2"></a>
+                                <a href="#modal-closed" class="link-2"></a>
                             </div>
                         </div>
                     </tbody>
@@ -95,7 +148,7 @@
             <div class="page-container">
                 <div class="pagination">
                     <a href="#">&laquo;</a>
-                    <a href="#">1</a>
+                    <a href="#" class="active">1</a>
                     <a href="#">2</a>
                     <a href="#">3</a>
                     <a href="#">4</a>
@@ -104,8 +157,6 @@
                     <a href="#">&raquo;</a>
                 </div>
             </div>
-
-            <h1>Games</h1>
 
         </main>
         {{-- <!-- END OF MAIN --> --}}
@@ -147,47 +198,17 @@
             <!-- ENF OF TOP -->
 
             <!-- START OF FILTERING -->
-            <div class="Due-Today">
-                <h2>Due Today</h2>
-                <div class="updates">
-                    <div class="update">
-                        <div class="profile-photo">
-                            <img src="https://scontent.fmnl3-4.fna.fbcdn.net/v/t39.30808-6/292629531_3183988248535102_7139051683664337622_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeGr3YEIOWfMfx1OIspeP6wIiKWwg7fpSceIpbCDt-lJx6WFojSUD7G_CFE93yAN7V_A1D-90Bmaaa3C825D-KtI&_nc_ohc=htKVaXCkJnoAX_vAoRx&_nc_zt=23&_nc_ht=scontent.fmnl3-4.fna&oh=00_AfDrfy-km54MZk4UxClhjovRczLvQz23grGY6znCBceHCg&oe=63788E25"
-                                alt="">
-                        </div>
-                        <div class="message">
-                            <p><b>Ceejay</b> 01-02-2022</p>
-                            <small class="text-muted">2 Minutes ago</small>
-                        </div>
-                    </div>
-                    <div class="update">
-                        <div class="profile-photo">
-                            <img src="https://scontent.fmnl3-4.fna.fbcdn.net/v/t39.30808-6/292629531_3183988248535102_7139051683664337622_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeGr3YEIOWfMfx1OIspeP6wIiKWwg7fpSceIpbCDt-lJx6WFojSUD7G_CFE93yAN7V_A1D-90Bmaaa3C825D-KtI&_nc_ohc=htKVaXCkJnoAX_vAoRx&_nc_zt=23&_nc_ht=scontent.fmnl3-4.fna&oh=00_AfDrfy-km54MZk4UxClhjovRczLvQz23grGY6znCBceHCg&oe=63788E25"
-                                alt="">
-                        </div>
-                        <div class="message">
-                            <p><b>Ceejay</b> 01-03-2022</p>
-                            <small class="text-muted">3 Minutes ago</small>
-                        </div>
-                    </div>
-                    <div class="update">
-                        <div class="profile-photo">
-                            <img src="https://scontent.fmnl3-4.fna.fbcdn.net/v/t39.30808-6/292629531_3183988248535102_7139051683664337622_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeGr3YEIOWfMfx1OIspeP6wIiKWwg7fpSceIpbCDt-lJx6WFojSUD7G_CFE93yAN7V_A1D-90Bmaaa3C825D-KtI&_nc_ohc=htKVaXCkJnoAX_vAoRx&_nc_zt=23&_nc_ht=scontent.fmnl3-4.fna&oh=00_AfDrfy-km54MZk4UxClhjovRczLvQz23grGY6znCBceHCg&oe=63788E25"
-                                alt="">
-                        </div>
-                        <div class="message">
-                            <p><b>Ceejay</b> 01-03-2022</p>
-                            <small class="text-muted">4 Minutes ago</small>
-                        </div>
+            <div class="filtering">
+                <h2>Filter</h2>
+                <div class="filters">
+                    <div class="filter">
+                        <label for="">Console</label>
                     </div>
                 </div>
             </div>
             <!-- END OF FILTERING -->
         </div>
         <script src="/js/index.js"></script>
-
-
-
 </body>
 
 </html>
