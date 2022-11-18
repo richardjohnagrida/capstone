8ba656b251af37ce1c2bae4d8e64f0083bc0a6df
senaCeejay <senaceejay2425@gmail.com>
2022-11-18 17:36:31 +0800

    pushing adding games tab, memberRenter pagination

--- gamerental/resources/views/layouts/memberRenter.blade.php
+++ gamerental/resources/views/layouts/memberRenter.blade.php
@@ -67,3 +67,15 @@
         </tbody>
     </table>
 </div>
+<div class="page-container">
+    <div class="pagination">
+        <a href="#">&laquo;</a>
+        <a href="#">1</a>
+        <a href="#">2</a>
+        <a href="#">3</a>
+        <a href="#">4</a>
+        <a href="#">5</a>
+        <a href="#">6</a>
+        <a href="#">&raquo;</a>
+    </div>
+</div>
