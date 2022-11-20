25e0ef0ba908843741c4d03fa27c445f37067bae
senaCeejay <senaceejay2425@gmail.com>
2022-11-20 19:36:30 +0800

    pushing login modal and js modal

--- gamerental/public/js/userJS.js
+++ gamerental/public/js/userJS.js
@@ -1,3 +1,24 @@
 $(".selectall").click(function () {
     $(".individual").prop("checked", $(this).prop("checked"));
 });
+
+
+var modal = document.getElementById("myModal");
+
+var btn = document.getElementById("myBtn");
+
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function () {
+    modal.style.display = "block"
+}
+
+span.onclick = function () {
+    modal.style.display = "none"
+}
+
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none"
+    }
+}
